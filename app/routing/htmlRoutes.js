5d55e869fc30ffa5827f0152978702b41643aac8
Sara Anspach <sara.anspach@gmail.com>
2018-09-17 16:49:42 -0400

    twelth

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,10 +2,15 @@ var path = require("path");
 module.exports = function(app) {
 	
 	// Routes
+	app.get("/home", function(_req, res) {
+		res.sendFile(path.join(__dirname, "/../public/home.html"));
+	});
+
 	app.get("/survey", function(_req, res) {
 		res.sendFile(path.join(__dirname, "/../public/survey.html"));
 	});
-	app.use(function (req, res){
-		res.sendFile(path.join(__dirname + "/../public/home.html"))
-	});
+
+	app.get("*", function(req, res) {
+		res.sendFile(path.join(__dirname, "/../public/home.html"));
+	  });
 };
